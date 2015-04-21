6fac3f3808d069499b8b127f5a1d2855de5e7ac4
Wojciech Barczynski <Wojciech.Barczynski@cloudandheat.com>
2015-04-21 03:00:49 +0200

    Refactoring and fixes

--- tools/metrics/overall_usage_stats.sh
+++ tools/metrics/overall_usage_stats.sh
@@ -2,38 +2,38 @@
 set -o nounset
 set -o errexit
 
-ENDPOINTS="hamm5" #;hamm6;dresden2"
+ENDPOINTS="hamm5;hamm6;dresden2"
 
-USAGE_START_DATE=2014-06-01
-USAGE_END_DATE=2014-12-01
+USAGE_START_DATE=2015-01-01
+USAGE_END_DATE=2015-04-04
 
 PRICE_XS_MONTH="14.60" #monthly
 PRICE_S_MONTH="29.20"
 PRICE_M_MONTH="58.40"
 PRICE_L_MONTH="102.20"
 
-INSTANCE_FILTER='leads-' # get only leads machiens
-INSTANCE_STATE='Running'
+INSTANCE_FILTER=leads- # get only leads machiens
+INSTANCE_STATE=Running
 
 echo $(echo $ENDPOINTS | tr ';' ' ')
 
-echo "| Servers | RAM MB-Hours | CPU Hours | Disk GB-Hours |"
-echo $ENDPOINTS | egrep -o '[^;]+' | xargs -I {} echo https://identity-{}.cloudandheat.com:5000/v2.0 |\
-	xargs -I {} nova --os-auth-url={} usage --start $USAGE_START_DATE --end $USAGE_END_DATE |\
-	grep '|' | egrep [0-9]+ 
-
+echo '|          FROM '$USAGE_START_DATE' TO '$USAGE_END_DATE'             |'
 
 flavors=""
 for ep in $(echo $ENDPOINTS | tr ";" "\n"); do
 	AUTH_URL="https://identity-${ep}.cloudandheat.com:5000/v2.0"
-	echo $ep
-	f=$(nova --os-auth-url=$AUTH_URL list | grep $INSTANCE_FILTER | grep $INSTANCE_STATE | cut -d'|' -f2 | tail -n +4 | grep -v '\-\-\-' | tr -d ' ' | tr '\n' '\000' |\
-	   xargs -P8 -0 -I {} nova --os-auth-url=$AUTH_URL show {} | grep flavor | cut -d'|' -f3)
-	echo $f
+
+    echo '|                  '$ep'                             |'
+    echo "| Servers | RAM MB-Hours | CPU Hours | Disk GB-Hours |"
+     nova --os-auth-url=${AUTH_URL} usage --start $USAGE_START_DATE --end $USAGE_END_DATE |\
+	    grep '|' | egrep [0-9]+ 
+	
+	f=$(nova --os-auth-url=$AUTH_URL list | grep -v '\-\-\-' | grep $INSTANCE_FILTER | grep $INSTANCE_STATE | cut -d'|' -f2 | tr -d ' '  | xargs -I {} nova --os-auth-url=$AUTH_URL show {} | grep flavor | cut -d'|' -f3)
 	flavors="${flavors} ${f}"
 done 
 
-# 'grep -c' exits 1 when no match
+#echo $flavors
+
 set +o errexit
 num_xs=$(echo "$flavors" | grep -c 'cloudcompute.xs')
 num_s=$(echo "$flavors"  | grep -c 'cloudcompute.s')
