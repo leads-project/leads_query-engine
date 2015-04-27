80fd16575671f6ad111c0426a808064b123da182
Wojciech Barczynski <Wojciech.Barczynski@cloudandheat.com>
2015-04-27 10:54:31 +0200

    Per default upload files from the last 24 hours

--- tools/metrics/backup/README.rst
+++ tools/metrics/backup/README.rst
@@ -4,6 +4,8 @@ Backup pcp to swift container
 
 (TODO: create salt for that)
 
+**Notice**: default it will upload only pcp files from the last 24 hours.
+
 1. Put the script *copy_pcp_to_swift.sh* to /home/ubuntu/metrics/backup 
 
 2. In /home/ubuntu/metrics/backup, run:
--- tools/metrics/backup/copy_pcp_to_swift.sh
+++ tools/metrics/backup/copy_pcp_to_swift.sh
@@ -9,9 +9,11 @@ PMLOGGER_BASE_DIR="/var/log/pcp/pmlogger"
 PMLOGGER_CONTAINER="experiments"
 HOSTNAME=$(hostname)
 
+PCP_MODIF_IN_LAST_24H="-mtime -1"
+
 pushd ${PMLOGGER_BASE_DIR}
 
-  for f in $(find ${HOSTNAME} -maxdepth 1 -mindepth 1 -type f); do
+  for f in $(find ${HOSTNAME} ${PCP_MODIF_IN_LAST_24H} -maxdepth 1 -mindepth 1 -type f); do
     swift upload --skip-identical --changed ${PMLOGGER_CONTAINER} $f --object-name=pcp/$f;
   done
 
