8ab9240fee97a58a3a1fae84a78535089acdc4dd
Wojciech Barczynski <Wojciech.Barczynski@cloudandheat.com>
2015-07-15 12:48:30 +0200

    Refactoring, adding task for loading data

--- fabric_experiements.py
+++ fabric_experiements.py
@@ -15,7 +15,7 @@ env.use_ssh_config = True
 YCSB_BINARY = "YCSB_PS.tar.gz"
 
 
-def install_ycsb():
+def ycsb_install():
     remote_file = "/home/ubuntu/{0}".format(YCSB_BINARY)
     if not exists(remote_file):
         utils.puts("Uploading {0}".format(YCSB_BINARY))
@@ -32,8 +32,34 @@ def _upload_with_scp(what, where):
         local("scp -F {0} {1} {2}:{3}".format(env.ssh_config_path, what, env.host_string, where))
 
 
-def run_ycsb_workloads_local():
+def yscb_load_workloads_local():
+    exp_time = time.strftime("%Y%m%d-%H%M%S")
+    for wl in ["workloada", "workloadb"]:
+        _exec_load(wl, exp_time)
 
+
+YCSB_RECORDCOUNT = 100000
+
+
+def _exec_load(workload, result_file_postfix):
+    with cd("/home/ubuntu/YCSB_PS"):
+        ispn_ip = run("hostname -I")
+        ispn_hn = run("hostname")
+        utils.puts("Load ycsb workload data to infinispan at {0}".format(ispn_ip))
+        result_file_name = "{0}-{1}-load-{2}.data".format(ispn_hn, workload, result_file_postfix)
+
+        with hide('running', 'stdout', 'stderr'):
+            out = run("./bin/ycsb load infinispan -P workloads/{1} -p host={0} -threads 10 -s -p recordcount={3} | tee -a {2}".format( 
+                      ispn_ip,
+                      workload,
+                      result_file_name,
+                      YCSB_RECORDCOUNT))
+            _print_results_to_console(out)
+            pwd = run("pwd")
+            _download_results_file(pwd, result_file_name)
+
+
+def ycsb_run_workloads_local():
     exp_time = time.strftime("%Y%m%d-%H%M%S")
 
     for wl in ["workloada", "workloadb"]:
@@ -46,7 +72,7 @@ def _exec_workload(workload, result_file_postfix):
         ispn_hn = run("hostname")
         utils.puts("Run ycsb against infinispan at {0}".format(ispn_ip))
 
-        result_file_name = "{0}-{1}-{2}.data".format(ispn_hn, workload, result_file_postfix)
+        result_file_name = "{0}-{1}-run-{2}.data".format(ispn_hn, workload, result_file_postfix)
         with hide('running', 'stdout', 'stderr'):
             out = run("./bin/ycsb run infinispan -P workloads/{1} -p host={0} -threads 10 -s | tee -a {2}".format(
                       ispn_ip,
@@ -70,7 +96,7 @@ def _print_results_to_console(out):
 def _download_results_file(r_dir, result_file):
     with hide('running', 'stdout'):
         local("scp -F {0} {1}:{2}/{3} {3}".format(
-            env.ssh_config_path, 
-            env.host_string, 
+            env.ssh_config_path,
+            env.host_string,
             r_dir,
             result_file))
