0b42482de4964b949fd68440b56315b64acf3506
Wojciech Barczynski <Wojciech.Barczynski@cloudandheat.com>
2015-11-08 13:49:48 +0100

    More sane way of harvesting tcpflow files and putting them to the swift container.

--- tools/metrics/backup/copy_tcpflow_to_swift.sh
+++ tools/metrics/backup/copy_tcpflow_to_swift.sh
@@ -19,13 +19,14 @@ MEASUREMENT_PREFIX="tcpflow/${HOSTNAME}"
 echo "MEASUREMENT FILES MTIME: ${MEASUREMENT_FILES_MTIME}"
 
 pushd ${BASE_DIR_WITH_MEASUREMENT}
-    for f in $(find * -maxdepth 1 -type f ${MEASUREMENT_MODIFIED_IN_LAST}); do
-        object_name=${MEASUREMENT_PREFIX}/$f;
-        # ensure that we do not overwrite the previous report file
-        if [ "$f" == "report.xml" ]; then
-            timestamp=$(cat report.xml | grep start_time | egrep -o "2[^<]+")
-            object_name=${object_name}"-"${timestamp}  
-        fi
-        swift upload --skip-identical --changed ${MEASUREMENT_CONTAINER} $f --object-name=${object_name};
-    done
+	find * -maxdepth 1 -type f ${MEASUREMENT_MODIFIED_IN_LAST} | xargs -I {} -P 3 swift upload --skip-identical --changed ${MEASUREMENT_CONTAINER} $f --object-name=${MEASUREMENT_PREFIX}{};
+
+    # for f in $(find * -maxdepth 1 -type f ${MEASUREMENT_MODIFIED_IN_LAST}); do
+    #    object_name=${MEASUREMENT_PREFIX}/$f;
+    #    # ensure that we do not overwrite the previous report file
+    #    if [ "$f" == "report.xml" ]; then
+    #        timestamp=$(cat report.xml | grep start_time | egrep -o "2[^<]+")
+    #        object_name=${object_name}"-"${timestamp}  
+    #    fi
+    # done
 popd
